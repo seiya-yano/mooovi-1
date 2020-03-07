9cac43b8aada0a925a0a7d1e8b15155203a88d30
seiya-yano <seyaseyaseiya@gmail.com>
2020-03-07 19:53:06 +0900

    テーブル追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_03_07_094622) do
+
+  create_table "products", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "title"
+    t.text "image_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "director"
+    t.text "detail"
+    t.string "open_date"
+  end
 
 end
