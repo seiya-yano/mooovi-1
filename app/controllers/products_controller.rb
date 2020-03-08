f9f2aa33c39ff8f8e1eb545a1b707672de3a2a6c
seiya-yano <seyaseyaseiya@gmail.com>
2020-03-08 21:14:31 +0900

    詳細画面表示まで完成

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,12 +1,11 @@
 class ProductsController < RankingController
   def index
-    # productsテーブルから最新順に作品を２０件取得する
     @products = Product.order('id ASC').limit(20)
   end
 
   def show
     # productsテーブルから該当するidの作品情報を取得し@productの変数へ代入する処理を書いて下さい
-    @product = Product.new # 問題3ではこのコードは消して新しくコードを書いてください
+    @product = Product.find(params[:id])
   end
 
   def search
